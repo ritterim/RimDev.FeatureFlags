c8bb7ae788874643e2ca71b81759cdcfcc4461fa
Thomas Harold <tom.harold@ritterim.com>
2022-06-03 09:54:03 -0400

    Add FeatureTests to validate Name/Description

--- tests/RimDev.AspNetCore.FeatureFlags.Tests/TestFeature.cs
+++ tests/RimDev.AspNetCore.FeatureFlags.Tests/TestFeature.cs
@@ -2,6 +2,6 @@ namespace RimDev.AspNetCore.FeatureFlags.Tests
 {
     public class TestFeature : Feature
     {
-        public override string Description { get; } = "Test feature.";
+        public override string Description { get; } = "Test feature description.";
     }
 }
--- tests/RimDev.AspNetCore.FeatureFlags.Tests/TestFeature2.cs
+++ tests/RimDev.AspNetCore.FeatureFlags.Tests/TestFeature2.cs
@@ -2,6 +2,6 @@ namespace RimDev.AspNetCore.FeatureFlags.Tests
 {
     public class TestFeature2 : Feature
     {
-        public override string Description { get; } = "Test feature 2.";
+        public override string Description { get; } = "Test feature 2 description.";
     }
 }
