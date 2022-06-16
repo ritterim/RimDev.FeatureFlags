5148b26dd32cfe5fb6d2a9bc2c6584b8a01bb83b
Thomas Harold <tom.harold@ritterim.com>
2022-06-16 08:23:40 -0400

    Move MapFeatureFlagsUI(), update XML comments

--- src/RimDev.AspNetCore.FeatureFlags.UI/UiStartupExtensions.cs
+++ src/RimDev.AspNetCore.FeatureFlags.UI/UiStartupExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RimDev.AspNetCore.FeatureFlags.UI
@@ -13,6 +15,16 @@ namespace RimDev.AspNetCore.FeatureFlags.UI
             return service;
         }
 
+        /// <summary>
+        /// <para>Uses <see cref="FeatureFlagsUiBuilder"/> to construct the default UI and API endpoints for feature
+        /// flags toggles.</para>
+        /// <para>Note that if you layer session managers, the value retrieved for a particular feature
+        /// may not match what is shown in this UI.  This UI/API only displays/updates the values for the
+        /// <see cref="FeatureFlagsSessionManager"/> session manager.</para>
+        /// <para>IMPORTANT: Controlling access of the UI / API of this library is the responsibility of the user.
+        /// Apply authentication / authorization around the `UseFeatureFlagsUI` method as needed, as this method
+        /// simply wires up the various endpoints.</para>
+        /// </summary>
         public static IApplicationBuilder UseRimDevFeatureFlagsUi(
             this IApplicationBuilder app
             )
@@ -44,6 +56,63 @@ namespace RimDev.AspNetCore.FeatureFlags.UI
             });
 
             return app;
+        }
+
+        /// <summary>
+        /// <para>Uses <see cref="IEndpointRouteBuilder"/> to map the UI/API endpoints.</para>
+        /// <para>Note that if you layer session managers, the value retrieved for a particular feature
+        /// may not match what is shown in this UI.  This UI/API only displays/updates the values for the
+        /// <see cref="FeatureFlagsSessionManager"/> session manager.</para>
+        /// <para>IMPORTANT: Controlling access of the UI / API of this library is the responsibility of the user.
+        /// Apply authentication / authorization around the `UseFeatureFlagsUI` method as needed, as this method
+        /// simply wires up the various endpoints.</para>
+        /// </summary>
+        public static IEndpointConventionBuilder MapFeatureFlagsUI(
+            this IEndpointRouteBuilder builder,
+            FeatureFlagsSettings settings,
+            FeatureFlagUiSettings uiSettings = default(FeatureFlagUiSettings)
+            )
+        {
+            if (settings is null) throw new ArgumentNullException(nameof(settings));
+
+            var featureFlagsUiBuilder = new FeatureFlagsUiBuilder();
+
+            return builder.Map(
+                uiSettings.UiPath + "/{**path}",
+                async context =>
+                {
+                    var path = context.Request.Path;
+
+                    if (path == uiSettings.ApiGetPath)
+                    {
+                        await featureFlagsUiBuilder.ApiGetPath(context, settings);
+                        return;
+                    }
+
+                    if (path == uiSettings.ApiGetAllPath)
+                    {
+                        await featureFlagsUiBuilder.ApiGetAllPath(context, settings);
+                        return;
+                    }
+
+                    if (path == uiSettings.ApiSetPath)
+                    {
+                        await featureFlagsUiBuilder.ApiSetPath(context, settings);
+                        return;
+                    }
+
+                    if (path == $"{uiSettings.UiPath}/main.js")
+                    {
+                        await context.Response.WriteManifestResource(typeof(UiStartupExtensions), "application/javascript", "main.js");
+                        return;
+                    }
+
+                    if (path == uiSettings.UiPath)
+                    {
+                        await context.Response.WriteManifestResource(typeof(UiStartupExtensions), "text/html", "index.html");
+                        return;
+                    }
+                });
         }
     }
 }
